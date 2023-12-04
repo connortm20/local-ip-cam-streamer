c8e1b32e47dbcef09651cf48795b32a4bc355ea8
Connor Marshall <connortm20@me.com>
2023-12-04 12:26:42 -0600

    new upload procedure

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ const fs =require('fs');
 const path = require('path');
 const express = require('express');
 const https = require('https');
-const options = { key: fs.readFileSync('/etc/ssl/localhost+2-key.pem'),
-		  cert: fs.readFileSync('/etc/ssl/localhost+2.pem')};
-//const options = { key: fs.readFileSync('server.key'),
-//		  cert: fs.readFileSync('server.crt')};
+//const options = { key: fs.readFileSync('/etc/ssl/localhost+2-key.pem'),
+//		  cert: fs.readFileSync('/etc/ssl/localhost+2.pem')};
+const options = { key: fs.readFileSync('server.key'),
+		  cert: fs.readFileSync('server.crt')};
 
-const yourAccessToken = "EwBwA8l6BAAUAOyDv0l6PcCVu89kmzvqZmkWABkAAQ589uZU1s4zpe2BPGY/uvvBIxeHQ0BQKblvokiNooHsTizbXk98vnCnYZ2O5FWzY24NTeuna7HZTDyYBqPbm5iop%2bhdWb3D93Xk6bbnzpXUlKWO0dzitiSJloHHuljyf1JGmFaXh5YhDCJY2Vsk086BJ%2buaQV25k6ceyUSEoJ95tZSyUOxVlMZAblmV2TLZAjw9zyd9EPSIBbeT52DwNztPBrYZdiC8v1kP/sSHOvpKmSSRn3bQHXBbgLwP8DdznIeGC1eB0f0tr9GEVbzrxsEEWDXk6c72gWw6anf%2bvFVoZrXE8f2K6t1WsMA0jpDJ77K5iK0jVzmGD7pyOnPIEGADZgAACFTVvFHUPzHwQAJDMwLh/mpgtpucKE4km1QL3z2S8r/575RWSHtVNXFzNSoy8RIkHmw1slMEHSq3tlg/iJwD9FHATvfZsoC3Coc1Cxl6u5Aek7uIEy09ifdhR2yIaA5aCLNutldMuHkVhD8uz0hklJVm1d07Y3eG0rF27v9bq56WiBnW9wcJMHlBQlBNhuZxlogNKSfSB8V9JzWXnNMzuzWpO7qTmwEFvO9uwHfpYow50dK7ufft89sBJttM4WbQnnafFdUUuDODnwMHWA0ElkucS5W4/W2DGY4qoLtgaBBCmbTETmBELn2jiK9U9tfrkNN/lOCKOAAsA/W3RIHFYCGnMeqJ2usqPZz0E%2bw/tW8Am8ocvh%2bPzOf9LBcBCC%2bT%2bp0FBKKLwhvbNAVVizGpZP/l8A/YWHLLYnYuqt93PudY1zGzl1hbDZAO7tWKH8wFWqJuvH8ch7eOBjrq/bgp4uFBEIGkOBoSsuEiKKEFqGKdNCnkUs0gbOoRejRjVO4LDW0RMRBuu64iZPb7xzVblKgcuKNo39T01Cya3x1yFkDPYRKheSAzag0cUQRwLB/RHnFz9ZMA1HyaWxQadjfpgTRQxFVIzJ3mRNyashOlGiswTagYJ8Jme%2b5bAKGJ6b9/FAyUVMvo0LqOg90ssHous7bnl/mPpeqDBRcmXkWESQigV2qr77vf7NtUzzhakMnbPShiaSaaMs6q4ssFTcZpbbShJvdKvQX2lcii690PMGaeaKLTh7580lcPRPhzHANrESxwQ94cIsNbbqZ1Ag%3d%3d"
+//manual request for new access
+//https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=85065df1-1a3c-466e-a0f9-0ef87e3badda&scope=files.readwrite&response_type=token&redirect_uri=https://localhost:5000/oauth-callback
+const yourAccessToken = "EwB4A8l6BAAUAOyDv0l6PcCVu89kmzvqZmkWABkAAbglooOdT7IUvl6Pj4jB5uOE/doLSl66XsEcTZCfDLVUC8L4nQ8a1otygVv9XSwcULNEe9cJGot/m4PA4Vah1Cof%2bfCFAclb/wRUS1Ygtfx9rNCwxXGagt%2b/6IdYLSsYsEP/tjwL6lcrnvf//wwtyKypf/jmNtSavXLegFqPNvnatqeUW8KdOkYr71Z8IrAPk54pAGZUUa8X5obul6uxAz8L96VNqy8CfkZVYJ3w07fDixT%2brO/xBSrW/bC3erTg45beEuQHJNAxxTJqQ9k6q40fNs6389M%2bzQkS6LgGw/iFozc9p3rTd4EFTdFN8ibbeCCpzwH3%2bz3qi1ZoN/nEwpgDZgAACAM8%2buzaT9yxSAKAyt7F6GvieusoxrQjFOIUbvdrF8gNnDDtcCsJv%2btAHgqbeUYoBz4ALgfBSyjBIIbS5MVi3MG/PnGpIF69e/%2bPOG6jZokZrkDxchlc/134aBiT05eP1nPj3jL7Xd4zhextSxkUXKK6oGrUvuJN4TBnfFm1YbqvxrOGRftVPdWgkK8ldaTaba9LAo033b8Nh/ewCq6W3Cj0OdK0XuWn%2blVGtcZgjHewmijGaQuB4J%2bYejh7vWMFa2p/jXHPfTZ5grKcegmIQY9pKqLGspJ8Gl9Eg%2bd0q54WF1PTMESmv%2b1EnyEFd%2bzY1e5zWfesqW8xKIbwwFNE8I0aV6m1CkFF1y%2b/kRDt6GzDkT/5fQkXTubD2BXXuo/1xSqVyy%2bnkkl5XviesgtBcbk4n4NZzS1GBLpIWQVR5tOvjSIVi2oVfviUaVj89ffGvyDHNuwaBrDzAcA0tK77MbZ2TbSaTH66IWiOkZ0M9aloET9FP2i0xXg4g0c6sCDO29ujAj1vsLCvtELvNcM3r4EFzNZ4LS%2bCuJ2jNcJu6P0s3sa3yLhIhF9%2bHts0OPvBh15SC0hr/FDOrWmAl6AAkTfFx4SWsFHWG2CKeW8XmqpMQZ3kxfHorwqaEbhbQTimLOctjbkZa53LB5qRE30VoGHibxtQq65Xaj0ZrqjFslO1wWK1CIwEWN1ctqNAdPXSqS3TH1htSG1y2scKy/e2hM96g6zI6EoVpdzgEPaeFsT4sGwnt5/Wd/Mqc4i6bDcI1NR3adj4oEd61uMui6pJ4K1ybXUC"
 
 const ffmpeg = require('fluent-ffmpeg');
 
